43a5fcbc1b25f7c37092fec24bfc15bc90425fde
Shahriar Rashid <srshahriar2020@gmail.com>
2026-04-09 10:00:30 +0600

    Initial commit - MediCare project

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+
+import { createRoot } from "react-dom/client";
+import App from "../UserInterface/App";
+import "../UserInterface/index.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
---
+++ src/routes.ts
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router";
+import { LandingPage } from "../UserInterface/pages/LandingPage";
+import { LoginPage } from "../UserInterface/pages/LoginPage";
+import { StudentDashboard } from "../UserInterface/pages/StudentDashboard";
+import { DoctorDashboard } from "../UserInterface/pages/DoctorDashboard";
+import { PharmacyDashboard } from "../UserInterface/pages/PharmacyDashboard";
+import { AdminDashboard } from "../UserInterface/pages/AdminDashboard";
+import { AmbulanceModule } from "../UserInterface/pages/AmbulanceModule";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: LandingPage,
+  },
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "/student-dashboard",
+    Component: StudentDashboard,
+  },
+  {
+    path: "/doctor-dashboard",
+    Component: DoctorDashboard,
+  },
+  {
+    path: "/pharmacy-dashboard",
+    Component: PharmacyDashboard,
+  },
+  {
+    path: "/admin-dashboard",
+    Component: AdminDashboard,
+  },
+  {
+    path: "/ambulance",
+    Component: AmbulanceModule,
+  },
+]);
